e9af9dda09870a706ffba87822e4ebf63ad629ac
ntnq4 <quentin.tardif@telecom-sudparis.eu>
2022-12-03 15:57:27 +0100

    second submission

--- model.py
+++ model.py
@@ -18,9 +18,10 @@ import os
 import torch
 from torch.nn import BatchNorm1d, Linear, Module, ReLU, Sequential
 from torch.nn import functional
-#from ctgan.data_transformer import SpanInfo
 import numpy as np
 from sdv.tabular import CTGAN
+import pandas as pd
+import pickle
 
 class Residual(Module):
 
@@ -87,13 +88,23 @@ def generative_model(noise):
         input noise of the generative model
     """
     # Load data transformations and output information
-    gan_model = CTGAN.load('./parameters/gan_model.pkl')
-    inverse_transform = gan_model._model._transformer.inverse_transform
-    output_info = gan_model._model._transformer.output_info_list
-    
+    #---MODIFY START---#
+    with open('./parameters/inverse_transform.pkl', 'rb') as f1:
+        inverse_transform = pickle.load(f1)
+
+    with open('./parameters/output_info.pkl', 'rb') as f2:
+        output_info = pickle.load(f2)
+    #---MODIFY END---#
+
     # Load generator
-    generator = Generator(embedding_dim=50, generator_dim=(256, 256, 256, 256, 128, 64, 32), data_dim=60)
-    generator.load_state_dict(torch.load('./parameters/generator'))
+    #---MODIFY START---#
+    generator = Generator(
+        embedding_dim=50, 
+        generator_dim=(256, 256, 256, 256, 256, 128, 128, 64), 
+        data_dim=61)
+    
+    generator.load_state_dict(torch.load('./parameters/new_generator.pkl', map_location=torch.device('cpu')))
+    #---MODIFY END---#
 
     # Generate data
     data = []
@@ -107,11 +118,41 @@ def generative_model(noise):
 
     data.rename(columns={'s1.value':'s1', 's2.value':'s2', 's3.value':'s3', 's4.value':'s4', 's5.value':'s5', 's6.value':'s6'}, inplace=True)
 
-    #Loading rescaling parameters
-    param = np.load('./parameters/rescaling_parameters_prev1.npy', allow_pickle=True)
-    mean = param.item()['mean']
-    std = param.item()['std']
+    # Loading rescaling parameters
+    #---MODIFY START---#
+    mean_rescale = pd.read_csv('./parameters/PredictedMean.csv').drop(columns=['year'])
+    std_rescale = pd.read_csv('./parameters/PredictedStd.csv').drop(columns=['year'])
 
-    data = data*std+mean
+    data_rescale = data.copy()
+
+    # Rescaling data
+    for y in range(9) :
+        mean = mean_rescale.iloc()[y]
+        std = std_rescale.iloc()[y]
+
+        i, j = 365*y, 365*(y+1)
+        data_rescale.iloc()[i:j] = data.iloc()[i:j]*std + mean
+
+    data_rescale.iloc()[3285:] = data.iloc()[3285:]*std + mean
     
-    return data
+    # TESTING
+    """
+    mean_train = pd.DataFrame.from_dict({'s1':[0.36908192814791435],
+                                         's2':[0.08642073719666811], 
+                                         's3':[0.49039190091339163],
+                                         's4':[0.34960050242313123],
+                                         's5':[0.35811780059425374],
+                                         's6':[0.7831963662360589]}).mean()
+
+    std_train = pd.DataFrame.from_dict({'s1':[1.3780324293187312],
+                                        's2':[1.840155294882411], 
+                                        's3':[1.6039499681791898],
+                                        's4':[1.2301866377403834],
+                                        's5':[1.7038051175878086],
+                                        's6':[1.7579832367606385]}).mean()
+
+    data_rescale = data.copy()*std_train+mean_train
+    """
+    #---MODIFY END---#
+
+    return data_rescale
